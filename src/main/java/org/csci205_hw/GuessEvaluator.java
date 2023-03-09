afe42b99cf862cfa835a78a5c237aa7e5cc60627
Thao Nguyen <tan007@bucknell.edu>
2023-03-09 17:45:38 -0500

    finished guessEvaluator

--- src/main/java/org/csci205_hw/GuessEvaluator.java
+++ src/main/java/org/csci205_hw/GuessEvaluator.java
@@ -18,6 +18,7 @@ package org.csci205_hw;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Scanner;
 import java.util.Set;
 
 public class GuessEvaluator{
@@ -28,6 +29,20 @@ public class GuessEvaluator{
     public static void main(String[] args) throws IOException {
         GuessEvaluator evaluate = new GuessEvaluator();
         evaluate.setSecretWord();
+        Scanner userIn = new Scanner(System.in);
+
+        int guessNum = 1;
+        while(true){
+            System.out.printf("Guess #%d: ", guessNum);
+            String userGuess = userIn.nextLine();
+            System.out.println("  -->     " + evaluate.analyzeGuess(userGuess));
+
+            if (userGuess.equals(evaluate.getSecretWord())){
+                System.out.printf("YOU WON! You guessed the word in %d turns!", guessNum);
+                break;
+            }
+            guessNum++;
+        }
 
 
     }
@@ -37,6 +52,14 @@ public class GuessEvaluator{
         this.guessAnalysis = guessAnalysis;
     }
 
+    private String getSecretWord(){
+        return secretWord;
+    }
+
+    /**
+     * Set the secret word from obtaining the methods from WordDictionary object methods
+     * @throws IOException
+     */
     public void setSecretWord() throws IOException {
         WordDictionary dict = new WordDictionary("words.txt");
         Set<String> wordSet = dict.readForUser();
@@ -49,18 +72,35 @@ public class GuessEvaluator{
         }
     }
 
+    /**
+     *
+     * @param guess
+     * @return string of -, +, or * depending on user input and if it aligns with secret word
+     */
     public String analyzeGuess(String guess){
         currentGuess = guess;
         guessAnalysis = "";
 
         if (currentGuess.length() != 5){
-            throw new IllegalArgumentException("5 letters please!")
+            throw new IllegalArgumentException("5 letters please!");
         }
 
         for (int i = 0; i < secretWord.length(); i++){
             char guessIndex = currentGuess.charAt(i);
+            char secretIndex = secretWord.charAt(i);
+
+            if (guessIndex == secretIndex){
+                guessAnalysis += "*";
+            }
+            else if (secretWord.indexOf(guessIndex) >= 0){
+                guessAnalysis += "+";
+            }
+            else{
+                guessAnalysis += "-";
+            }
 
         }
+        return guessAnalysis;
     }
 }
 
